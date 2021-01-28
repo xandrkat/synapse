944a1e674b4025c56d2260032649b3cabbcbf6d7
Bruno Windels <brunow@element.io>
2021-01-28 13:45:05 +0100

    wip for adding account details to copy over

--- synapse/res/username_picker/script.js
+++ synapse/res/username_picker/script.js
@@ -1,4 +1,4 @@
-let inputField = document.getElementById("field-username");
+let usernameField = document.getElementById("field-username");
 let inputForm = document.getElementById("form");
 let submitButton = document.getElementById("button-submit");
 let message = document.getElementById("message");
@@ -11,85 +11,52 @@ function showMessage(messageText) {
     message.textContent = messageText;
 };
 
-function doSubmit() {
-    showMessage("Success. Please wait a moment for your browser to redirect.");
-
-    // remove the event handler before re-submitting the form.
-    delete inputForm.onsubmit;
-    inputForm.submit();
-}
-
-function onResponse(response) {
-    // Display message
-    showMessage(response);
-
-    // Enable submit button and input field
-    submitButton.classList.remove('button--disabled');
-    submitButton.value = "Submit";
-};
-
 let allowedUsernameCharacters = RegExp("[^a-z0-9\\.\\_\\=\\-\\/]");
-function usernameIsValid(username) {
-    return !allowedUsernameCharacters.test(username);
-}
 let allowedCharactersString = "lowercase letters, digits, ., _, -, /, =";
-
-function buildQueryString(params) {
-    return Object.keys(params)
-        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-        .join('&');
-}
-
-function submitUsername(username) {
-    if(username.length == 0) {
-        onResponse("Please enter a username.");
+usernameField.addEventListener("change", function(evt) {
+    usernameField.setCustomValidity("");
+    const username = usernameField.value;
+    if (usernameField.validity.valueMissing) {
+        usernameField.setCustomValidity("Please provide a username");
         return;
     }
-    if(!usernameIsValid(username)) {
-        onResponse("Invalid username. Only the following characters are allowed: " + allowedCharactersString);
+    if (usernameField.validity.patternMismatch) {
+        usernameField.setCustomValidity("Invalid username " + username + ". Only the following characters are allowed: " + allowedCharactersString);
         return;
     }
+    try {
 
-    // if this browser doesn't support fetch, skip the availability check.
-    if(!window.fetch) {
-        doSubmit();
-        return;
+        checkUsernameAvailable(username).then(function(result) {
+            if (!result.available) {
+                usernameField.setCustomValidity(result.message);
+            }
+        }, function(err) {
+            showMessage(err.message);
+        });
+    } catch (err) {
+        showMessage("Could not verify ");
     }
+});
 
-    let check_uri = 'check?' + buildQueryString({"username": username});
-    fetch(check_uri, {
+function checkUsernameAvailable(username) {
+    let check_uri = 'check?username=' + encodeURIComponent(username);
+    return feetch(check_uri, {
         // include the cookie
         "credentials": "same-origin",
     }).then((response) => {
         if(!response.ok) {
             // for non-200 responses, raise the body of the response as an exception
-            return response.text().then((text) => { throw text; });
+            return response.text().then((text) => { throw new Error(text); });
         } else {
             return response.json();
         }
     }).then((json) => {
         if(json.error) {
-            throw json.error;
+            return {message: json.error};
         } else if(json.available) {
-            doSubmit();
+            return {available: true};
         } else {
-            onResponse("This username is not available, please choose another.");
+            return {message: "This username is not available, please choose another."};
         }
-    }).catch((err) => {
-        onResponse("Error checking username availability: " + err);
     });
 }
-
-function clickSubmit() {
-    event.preventDefault();
-    if(submitButton.classList.contains('button--disabled')) { return; }
-
-    // Disable submit button and input field
-    submitButton.classList.add('button--disabled');
-
-    // Submit username
-    submitButton.value = "Checking...";
-    submitUsername(inputField.value);
-};
-
-inputForm.onsubmit = clickSubmit;
